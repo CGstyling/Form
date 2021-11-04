40d93c8815ab448b3e70cdf61346ea45323be8c8
CGstyling <christinaguseva@gmail.com>
2021-11-04 10:22:07 +0100

    Form

--- src/App.js
+++ src/App.js
@@ -1,24 +1,95 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
+
 function App() {
+  const [state, setState] = React.useState({
+    name: "",
+    age:"",
+    comments: "",
+    check: false
+  });
+
+  function handleChange(e) {
+    const value =
+       e.target.type === "checkbox" ? e.target.checked : e.target.value;
+    setState({
+      ...state,
+      [e.target.name]:value
+    });
+  }
+
+  function handleClick(e) {
+    e.preventDefault();
+    console.log(state);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <>
+        <form onSubmit={handleClick}>
+          <fieldset>
+            <legend><h2>Gegevens:</h2></legend>
+            <label htmlFor="naam">
+              Name:
+              <input
+                  type="text"
+                  id="naam"
+                  name="name"
+                  value={state.name}
+                  onChange={handleChange}
+                  placeholder="Type your name here..."
+              />
+            </label>
+
+            <br/>
+
+            <label htmlFor="leeftijd">
+              Leeftijd:
+              <input
+                  type="number"
+                  id="leeftijd"
+                  name="age"
+                  value={state.age}
+                  onChange={handleChange}
+              />
+            </label>
+          </fieldset>
+
+          <fieldset>
+            <legend><h2>Jouw review</h2></legend>
+
+            <label htmlFor="text">
+              <p>comments:</p>
+              <textarea
+                  name="comments"
+                  id="text"
+                  cols="45"
+                  rows="6"
+                  placeholder="Hoe vond je het recept?"
+                  value={state.comments}
+                  onChange={handleChange}>
+                </textarea>
+            </label>
+
+            <br/>
+
+            <label htmlFor="form">
+              <input
+                  type="checkbox"
+                  id="form"
+                  name="check"
+                  checked={state.check}
+                  onChange={handleChange}
+              />
+              Ik schrijf mij in voor de nieuwsbrief
+            </label>
+
+            <button type="submit">
+              Versturen
+            </button>
+          </fieldset>
+        </form>
+      </>
   );
 }
 
